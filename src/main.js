0c380b61a9abf2b045b7240d77102fbd8dc34323
Rumpel <hades137747575@gmail.com>
2017-07-02 20:48:49 +0800

    import vue-awesome

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'vue-awesome/icons/flag'
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
 
 Vue.config.productionTip = false
 
@@ -17,3 +20,5 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
+
+Vue.component('icon', Icon)
