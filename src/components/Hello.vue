abbe64c02a0cc8541f973c0fce476ade3d0575ec
Rumpel <hades137747575@gmail.com>
2017-07-03 21:27:03 +0800

    clean

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,5 +1,3 @@
 <template src="./template.html"></template>
 
 <script src="./script.js"></script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-//  模块引入
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -8,7 +7,6 @@ import BootstrapVue from 'bootstrap-vue'
 import Icon from 'vue-awesome/components/Icon'
 import Vuex from 'vuex'
 import VueResource from 'vue-resource'
-//  样式引入
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'vue-awesome/icons/flag'
