7f2d0c8041a4a047d516c7f95da1ddc65c5d6658
Rumpel <hades137747575@gmail.com>
2017-07-03 15:55:35 +0800

    Structural adjustment

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+//  模块引入
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
+import Icon from 'vue-awesome/components/Icon'
+//  样式引入
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'vue-awesome/icons/flag'
 import 'vue-awesome/icons'
-import Icon from 'vue-awesome/components/Icon'
+import './sass/main.scss'
+
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 /* eslint-disable no-new */
