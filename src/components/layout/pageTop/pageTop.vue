9ebf30fd80dbe89f96bdabfce32acf11ad07b16d
Rumpel <hades137747575@gmail.com>
2017-07-06 22:09:57 +0800

    single-file components

--- src/components/layout/pageTop/pageTop.vue
+++ src/components/layout/pageTop/pageTop.vue
@@ -1,3 +1,33 @@
-<template src="./template.html"></template>
+<template>
+  <div class="page-top clearfix" v-scroll-position >
+    <a href="#/" class="al-logo clearfix"><span>Vue</span>Admin</a>
+    <a href class="collapse-menu-link ion-navicon" ></a>
 
-<script src="./script.js"></script>
+    <div class="search">
+      <i class="ion-ios-search-strong" ></i>
+      <input id="searchInput" type="text" placeholder="Search for...">
+    </div>
+
+    <div class="user-profile clearfix">
+      <div class="al-user-profile" u>
+        <a class="profile-toggle-link">
+        </a>
+        <ul  class="top-dropdown-menu profile-dropdown" >
+          <li><i class="dropdown-arr"></i></li>
+          <li><a href="#/profile"><i class="fa fa-user"></i>Profile</a></li>
+          <li><a href><i class="fa fa-cog"></i>Settings</a></li>
+          <li><a href class="signout"><i class="fa fa-power-off"></i>Sign out</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import scrollPosition from './scrollPosition'
+  export default {
+    name: 'pageTop',
+    directives: { scrollPosition }
+  }
+
+</script>
--- src/components/layout/pageTop/script.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Cai Kang Jie on 2017/7/3.
- */
-import scrollPosition from './scrollPosition'
-export default {
-  name: 'pageTop',
-  directives: { scrollPosition }
-}
