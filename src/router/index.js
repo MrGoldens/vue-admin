657f4a9fd2819848f8703565ac466f2f8508e748
Rumpel <hades137747575@gmail.com>
2017-07-09 11:31:17 +0800

    new routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,23 +2,25 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import dashboard from '../pages/dashboard/index.vue'
 import dashboard2 from '../pages/dashboard2/index.vue'
-Vue.use (Router)
+Vue.use(Router)
 
-export default new Router ({
-  routes: [
-    {
-      name: 'Dashboard',
-      path: '/',
-      component: dashboard
-    },
-    {
-      path: '*',
-      redirect: '/'
-    },
-    {
-      name: 'dashboard2',
-      path: '/',
-      component: dashboard2
-    }
-  ]
+const routes = [
+  {
+    name: 'Dashboard',
+    path: '/',
+    component: dashboard
+  },
+  {
+    path: '*',
+    redirect: '/home'
+  },
+  {
+    name: 'home',
+    path: '/home',
+    component: dashboard2
+  }
+]
+
+export default new Router({
+  routes: routes
 })
