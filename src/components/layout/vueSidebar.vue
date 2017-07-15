0be0821879db0d871834ba02cd68a3f14473ac6a
Rumpel <hades137747575@gmail.com>
2017-07-15 11:40:34 +0800

    sidebar repeat list

--- src/components/layout/vueSidebar.vue
+++ src/components/layout/vueSidebar.vue
@@ -1,6 +1,12 @@
 <template>
   <aside class="al-sidebar">
-
+    <ul class="al-sidebar-list">
+      <li class="al-sidebar-list-item" v-for="item in routeConfig">
+        <a class="al-sidebar-list-link">
+          <i :class="item.sidebarMeta.icon"></i><span>{{item.name}}</span>
+        </a>
+      </li>
+    </ul>
   </aside>
 </template>
 
--- src/store/modules/routeConfig/index.js
+++ src/store/modules/routeConfig/index.js
@@ -18,7 +18,12 @@ const state = {
     {
       name: 'Dashboard2',
       path: '/dashboard2',
-      component: lazyLoading('dashboard2', true)
+      component: lazyLoading('dashboard2', true),
+      sidebarMeta: {
+        title: 'dashboard',
+        icon: 'ion-android-home',
+        order: 1
+      }
     }
   ]
 }
