3a02017a9094c504e78a2e2b5a6617095b529706
Rumpel <hades137747575@gmail.com>
2017-07-23 20:15:55 +0800

    expanded level2 menu

--- src/components/layout/vueSidebar.vue
+++ src/components/layout/vueSidebar.vue
@@ -9,7 +9,7 @@
           <i :class="item.sidebarMeta.icon"></i><span>{{item.name}}</span>
           <b class="fa" :class="{'fa-angle-up': item.expanded, 'fa-angle-down': !item.expanded}" ></b>
         </a>
-        <ul class="al-sidebar-sublist" v-if="item.subMenu" :class="{'slide-right': item.slideRight}">
+        <ul class="al-sidebar-sublist" v-if="item.subMenu" :class="{'slide-right': item.slideRight,'expanded': item.expanded}">
           <li class="ba-sidebar-sublist-item" v-for="subitem in item.subMenu" :class="{'with-sub-menu':subitem.subMenu,'selected':subitem.path===sidebar.selected}">
             <router-link :to="subitem.path" class="al-sidebar-list-link subitem-submenu-link" >
               <span>{{subitem.name}}</span>
