df8ec86331b3319f9fc0c3f4419759f5fe8a0135
Rumpel <hades137747575@gmail.com>
2017-08-08 15:39:47 +0800

    install bootstrap select tag-input switch

--- src/main.js
+++ src/main.js
@@ -5,19 +5,18 @@ import App from './App'
 import router from './router'
 import store from './store'
 import { SIDEBAR_SELECTED } from './store/mutation-types'
-import BootstrapVue from 'bootstrap-vue'
+// import BootstrapVue from 'bootstrap-vue'
 import Icon from 'vue-awesome/components/Icon'
 import Vuex from 'vuex'
 import VueResource from 'vue-resource'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'vue-awesome/icons/flag'
 import 'vue-awesome/icons'
 import './sass/main.scss'
 import 'ionicons/scss/ionicons.scss'
 
 Vue.config.productionTip = false
-Vue.use(BootstrapVue)
+// Vue.use(BootstrapVue)
 Vue.use(Vuex)
 Vue.use(VueResource)
 Vue.component('icon', Icon)
