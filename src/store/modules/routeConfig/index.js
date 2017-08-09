ed32da8883bc092ff11435b0ce13afe77d023a77
Rumpel <hades137747575@gmail.com>
2017-08-09 15:19:04 +0800

    clean project

--- src/store/modules/routeConfig/index.js
+++ src/store/modules/routeConfig/index.js
@@ -17,16 +17,6 @@ const state = {
       }
     },
     {
-      name: 'Dashboard2',
-      path: '/dashboard2',
-      component: lazyLoading('dashboard2', true),
-      sidebarMeta: {
-        title: 'dashboard',
-        icon: 'ion-android-home',
-        order: 1
-      }
-    },
-    {
       name: 'Components',
       path: '/ccc',
       component: lazyLoading('dashboard2', true),
