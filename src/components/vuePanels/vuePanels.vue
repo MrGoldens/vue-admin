21d3da44da3fc72da46bce0d89270f97df8319f1
Rumpel <hades137747575@gmail.com>
2017-08-11 13:08:48 +0800

    panel background position

--- src/components/vuePanels/vuePanels.vue
+++ src/components/vuePanels/vuePanels.vue
@@ -16,26 +16,27 @@
       let computedStyle = getComputedStyle(document.body, ':before')
       let image = new Image()
       image.src = computedStyle.backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2')
-      function getBodyBgImageSizes () {
-        let elemW = document.documentElement.clientWidth
-        let elemH = document.documentElement.clientHeight
-        if (elemW <= 640) return
-        let imgRatio = (image.height / image.width)       // original img ratio
-        let containerRatio = (elemH / elemW)     // container ratio
-
-        let finalHeight, finalWidth
-        if (containerRatio > imgRatio) {
-          finalHeight = elemH
-          finalWidth = (elemH / imgRatio)
-        } else {
-          finalWidth = elemW
-          finalHeight = (elemW * imgRatio)
-        }
-        console.log({width: finalWidth, height: finalHeight, positionX: (elemW - finalWidth) / 2, positionY: (elemH - finalHeight) / 2})
-        return {width: finalWidth, height: finalHeight, positionX: (elemW - finalWidth) / 2, positionY: (elemH - finalHeight) / 2}
-      }
       getBodyBgImageSizes()
     }
+  }
+  //get image size
+  function getBodyBgImageSizes () {
+    let elemW = document.documentElement.clientWidth
+    let elemH = document.documentElement.clientHeight
+    if (elemW <= 640) return
+    let imgRatio = (image.height / image.width)       // original img ratio
+    let containerRatio = (elemH / elemW)     // container ratio
+
+    let finalHeight, finalWidth
+    if (containerRatio > imgRatio) {
+      finalHeight = elemH
+      finalWidth = (elemH / imgRatio)
+    } else {
+      finalWidth = elemW
+      finalHeight = (elemW * imgRatio)
+    }
+    console.log({width: finalWidth, height: finalHeight, positionX: (elemW - finalWidth) / 2, positionY: (elemH - finalHeight) / 2})
+    return {width: finalWidth, height: finalHeight, positionX: (elemW - finalWidth) / 2, positionY: (elemH - finalHeight) / 2}
   }
 </script>
 
