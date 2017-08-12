acabb2a93d6f98ef1f5a78936f595c0d2492f63f
Rumpel <hades137747575@gmail.com>
2017-08-12 18:01:21 +0800

    vue panels

--- src/components/vuePanels/vuePanels.vue
+++ src/components/vuePanels/vuePanels.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="panel panel-blur">
+<div class="panel panel-blur" :style="bgStyle">
   <div class="panel-body">
     <span>
       <slot></slot>
@@ -12,11 +12,11 @@
 //  import $ from 'jquery'
   export default {
     name: 'vuePanel',
-    mounted () {
-      let computedStyle = getComputedStyle(document.body, ':before')
-      let image = new Image()
-      image.src = computedStyle.backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2')
-      function getBodyBgImageSizes () {
+    methods: {
+      getBodyBgImageSizes () {
+        let computedStyle = getComputedStyle(document.body, ':before')
+        let image = new Image()
+        image.src = computedStyle.backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2')
         let elemW = document.documentElement.clientWidth
         let elemH = document.documentElement.clientHeight
         if (elemW <= 640) return
@@ -32,12 +32,17 @@
           finalHeight = (elemW * imgRatio)
         }
         console.log({width: finalWidth, height: finalHeight, positionX: (elemW - finalWidth) / 2, positionY: (elemH - finalHeight) / 2})
-        return {width: finalWidth, height: finalHeight, positionX: (elemW - finalWidth) / 2, positionY: (elemH - finalHeight) / 2}
+        console.log({backgroundSize: finalWidth + 'px' + ' ' + finalHeight + 'px', backgroundPosition: ((elemW - finalWidth) / 2) + 'px' + ' ' + ((elemH - finalHeight) / 2) + 'px'})
+//        return {width: finalWidth, height: finalHeight, positionX: (elemW - finalWidth) / 2, positionY: (elemH - finalHeight) / 2}
+        return {backgroundSize: finalWidth + 'px' + ' ' + finalHeight + 'px', backgroundPosition: ((elemW - finalWidth) / 2) + 'px' + ' ' + ((elemH - finalHeight) / 2) + 'px'}
       }
-      getBodyBgImageSizes()
+    },
+    data () {
+      return {
+        bgStyle: this.getBodyBgImageSizes()
+      }
     }
   }
-
 </script>
 
 <style></style>
