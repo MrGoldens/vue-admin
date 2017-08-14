7fa3c455286ff428d35c178250eacb0bd3f47fe3
Rumpel <hades137747575@gmail.com>
2017-08-14 17:21:55 +0800

    panel blur background style

--- src/components/vuePanels/panelBlurBgStyle.js
+++ src/components/vuePanels/panelBlurBgStyle.js
@@ -1,3 +1,13 @@
 /**
  * Created by Cai Kang Jie on 2017/8/14.
  */
+// import $ from 'jquery'
+export default {
+  name: 'panelBlurBgStyle',
+  bind (el, binding, vnode, oldVnode) {
+    console.log(el)
+    console.log(binding)
+    console.log(vnode)
+    console.log(oldVnode)
+  }
+}
--- src/components/vuePanels/vuePanels.vue
+++ src/components/vuePanels/vuePanels.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="panel panel-blur" :style="bgStyle">
+<div class="panel panel-blur" v-panel-blur-bg-style>
   <div class="panel-heading clearfix" v-show="vuePanelTitle!=null">
     <h3 class="panel-title">{{vuePanelTitle}}</h3>
   </div>
@@ -13,40 +13,42 @@
 
 <script>
 //  import $ from 'jquery'
-  export default {
-    name: 'vuePanel',
-    methods: {
-      getBodyBgImageSizes () {
-        let computedStyle = getComputedStyle(document.body, ':before')
-        let image = new Image()
-        image.src = computedStyle.backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2')
-        let elemW = document.documentElement.clientWidth
-        let elemH = document.documentElement.clientHeight
-        if (elemW <= 640) return
-        let imgRatio = (image.height / image.width)       // original img ratio
-        let containerRatio = (elemH / elemW)     // container ratio
+import panelBlurBgStyle from './panelBlurBgStyle'
+export default {
+  name: 'vuePanel',
+  directives: { panelBlurBgStyle },
+  methods: {
+    getBodyBgImageSizes () {
+      let computedStyle = getComputedStyle(document.body, ':before')
+      let image = new Image()
+      image.src = computedStyle.backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2')
+      let elemW = document.documentElement.clientWidth
+      let elemH = document.documentElement.clientHeight
+      if (elemW <= 640) return
+      let imgRatio = (image.height / image.width)       // original img ratio
+      let containerRatio = (elemH / elemW)     // container ratio
 
-        let finalHeight, finalWidth
-        if (containerRatio > imgRatio) {
-          finalHeight = elemH
-          finalWidth = (elemH / imgRatio)
-        } else {
-          finalWidth = elemW
-          finalHeight = (elemW * imgRatio)
-        }
-        return {
-          backgroundSize: finalWidth + 'px' + ' ' + finalHeight + 'px',
-          backgroundPosition: ((elemW - finalWidth) / 2) + 'px' + ' ' + ((elemH - finalHeight) / 2) + 'px'
-        }
+      let finalHeight, finalWidth
+      if (containerRatio > imgRatio) {
+        finalHeight = elemH
+        finalWidth = (elemH / imgRatio)
+      } else {
+        finalWidth = elemW
+        finalHeight = (elemW * imgRatio)
       }
-    },
-    data () {
       return {
-        bgStyle: this.getBodyBgImageSizes()
+        backgroundSize: finalWidth + 'px' + ' ' + finalHeight + 'px',
+        backgroundPosition: ((elemW - finalWidth) / 2) + 'px' + ' ' + ((elemH - finalHeight) / 2) + 'px'
       }
-    },
-    props: ['vuePanelTitle']
-  }
+    }
+  },
+  data () {
+    return {
+      bgStyle: this.getBodyBgImageSizes()
+    }
+  },
+  props: ['vuePanelTitle']
+}
 </script>
 
 <style></style>
