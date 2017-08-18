510fe87b8defac5d501e98cbe1235832ec541da9
Rumpel <hades137747575@gmail.com>
2017-08-18 11:32:51 +0800

    Raised Buttons

--- src/pages/ui/buttons.vue
+++ src/pages/ui/buttons.vue
@@ -1,22 +1,26 @@
 <template>
   <div class="widgets">
     <div class="row">
-      <vue-panel class="col-md-3 with-scroll button-panel" vue-panel-title="Flat Buttons">
-        <div class="button-wrapper"><button type="button" class="btn btn-default">Default</button></div>
-        <div class="button-wrapper"><button type="button" class="btn btn-primary">Primary</button></div>
-        <div class="button-wrapper"><button type="button" class="btn btn-success">Success</button></div>
-        <div class="button-wrapper"><button type="button" class="btn btn-info">Info</button></div>
-        <div class="button-wrapper"><button type="button" class="btn btn-warning">Warning</button></div>
-        <div class="button-wrapper"><button type="button" class="btn btn-danger">Danger</button></div>
-      </vue-panel>
-      <vue-panel class="col-md-3 with-scroll button-panel" vue-panel-title="Raised Buttons">
-        <div class="button-wrapper"><button type="button" class="btn btn-default btn-raised">Default</button></div>
-        <div class="button-wrapper"><button type="button" class="btn btn-primary btn-raised">Primary</button></div>
-        <div class="button-wrapper"><button type="button" class="btn btn-success btn-raised">Success</button></div>
-        <div class="button-wrapper"><button type="button" class="btn btn-info btn-raised">Info</button></div>
-        <div class="button-wrapper"><button type="button" class="btn btn-warning btn-raised">Warning</button></div>
-        <div class="button-wrapper"><button type="button" class="btn btn-danger btn-raised">Danger</button></div>
-      </vue-panel>
+      <div class="col-md-3">
+        <vue-panel class="with-scroll button-panel" vue-panel-title="Flat Buttons">
+          <div class="button-wrapper"><button type="button" class="btn btn-default">Default</button></div>
+          <div class="button-wrapper"><button type="button" class="btn btn-primary">Primary</button></div>
+          <div class="button-wrapper"><button type="button" class="btn btn-success">Success</button></div>
+          <div class="button-wrapper"><button type="button" class="btn btn-info">Info</button></div>
+          <div class="button-wrapper"><button type="button" class="btn btn-warning">Warning</button></div>
+          <div class="button-wrapper"><button type="button" class="btn btn-danger">Danger</button></div>
+        </vue-panel>
+      </div>
+      <div class="col-md-3">
+        <vue-panel class="with-scroll button-panel" vue-panel-title="Raised Buttons">
+          <div class="button-wrapper"><button type="button" class="btn btn-default btn-raised">Default</button></div>
+          <div class="button-wrapper"><button type="button" class="btn btn-primary btn-raised">Primary</button></div>
+          <div class="button-wrapper"><button type="button" class="btn btn-success btn-raised">Success</button></div>
+          <div class="button-wrapper"><button type="button" class="btn btn-info btn-raised">Info</button></div>
+          <div class="button-wrapper"><button type="button" class="btn btn-warning btn-raised">Warning</button></div>
+          <div class="button-wrapper"><button type="button" class="btn btn-danger btn-raised">Danger</button></div>
+        </vue-panel>
+      </div>
     </div>
   </div>
 </template>
