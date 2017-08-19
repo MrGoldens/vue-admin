63b7dda78d25efe3c119e227ece35e2e91951d87
Rumpel <hades137747575@gmail.com>
2017-08-19 22:01:13 +0800

    Progress Bars

--- src/pages/ui/progressBars.vue
+++ src/pages/ui/progressBars.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="widgets">
     <div class="row">
+      <div class="col-md-6">
+        <vue-panel vue-panel-title="Basic" class="with-scroll">
 
+        </vue-panel>
+        <vue-panel vue-panel-title="Striped" class="with-scroll">
+
+        </vue-panel>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {}
+  import VuePanel from '../../components/vuePanels/vuePanels.vue'
+
+  export default {components: {VuePanel}}
 
 </script>
 
