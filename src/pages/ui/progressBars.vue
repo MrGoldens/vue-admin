956243d94c3d5869a61cee62fa3a23f09c0d4633
Rumpel <hades137747575@gmail.com>
2017-08-19 22:04:38 +0800

    Progress Bars

--- src/pages/ui/progressBars.vue
+++ src/pages/ui/progressBars.vue
@@ -95,7 +95,24 @@
       </div>
     </div>
     <div class="row">
-
+      <div class="col-md-12">
+        <vue-panel vue-panel-title="Stacked" class="with-scroll">
+          <div class="progress">
+            <div class="progress-bar progress-bar-success" style="width: 35%">
+              <span class="sr-only">35% Complete (success)</span>
+            </div>
+            <div class="progress-bar progress-bar-warning progress-bar-striped" style="width: 20%">
+              <span class="sr-only">20% Complete (warning)</span>
+            </div>
+            <div class="progress-bar progress-bar-danger" style="width: 10%">
+              <span class="sr-only">10% Complete (danger)</span>
+            </div>
+            <div class="progress-bar progress-bar-info progress-bar-striped active" style="width: 20%">
+              <span class="sr-only">20% Complete (warning)</span>
+            </div>
+          </div>
+        </vue-panel>
+      </div>
     </div>
   </div>
 </template>
