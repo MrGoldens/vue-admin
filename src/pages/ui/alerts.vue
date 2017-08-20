f7515839973e998c2ff42eb8e149ffeaeac074ba
Rumpel <hades137747575@gmail.com>
2017-08-20 20:22:09 +0800

    alerts

---
+++ src/pages/ui/alerts.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="widgets">
+    <div class="row">
+      <div class="col-md-6">
+        <vue-panel vue-panel-title="Basic" class="with-scroll">
+          <div >
+            <div class="alert bg-success">
+              <strong>Well done!</strong> You successfully read this important alert message.
+            </div>
+            <div class="alert bg-info">
+              <strong>Heads up!</strong> This alert needs your attention, but it's not super important.
+            </div>
+            <div class="alert bg-warning">
+              <strong>Warning!</strong> Better check yourself, you're not looking too good.
+            </div>
+            <div class="alert bg-danger">
+              <strong>Oh snap!</strong> Change a few things up and try submitting again.
+            </div>
+          </div>
+        </vue-panel>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import VuePanel from '../../components/vuePanels/vuePanels.vue'
+
+  export default {components: {VuePanel}}
+
+</script>
+
+<style></style>
