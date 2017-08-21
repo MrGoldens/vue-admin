1068942d9c9cdb0912575d79ecd0df4433015205
Rumpel <hades137747575@gmail.com>
2017-08-21 20:01:24 +0800

    Form Inputs

--- src/pages/form/index.js
+++ src/pages/form/index.js
@@ -1,3 +1,22 @@
 /**
  * Created by Cai Kang Jie on 2017/7/31.
  */
+import lazyLoading from './../../store/modules/routeConfig/lazyLoading'
+export default {
+  name: 'Form Elements',
+  expanded: false,
+  sidebarMeta: {
+    title: 'Form Elements',
+    icon: 'ion-android-laptop',
+    order: 1
+  },
+  subMenu: [{
+    name: 'inputs',
+    path: '/form/inputs',
+    component: lazyLoading('ui/inputs'),
+    sidebarMeta: {
+      title: 'Form Inputs',
+      order: 100
+    }
+  }]
+}
