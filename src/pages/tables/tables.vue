aaaf6e65b89731a09d1ceda9f4daa9becaadc0d6
Rumpel <hades137747575@gmail.com>
2017-08-29 20:06:04 +0800

    Condensed Table

--- src/pages/tables/tables.vue
+++ src/pages/tables/tables.vue
@@ -132,6 +132,53 @@
             percent: '5%',
             isPercentUp: false
           }
+        ],
+        peopleTableData: [
+          {
+            id: 1,
+            firstName: 'Mark',
+            lastName: 'Otto',
+            username: '@mdo',
+            email: 'mdo@gmail.com',
+            age: '28',
+            status: 'info'
+          },
+          {
+            id: 2,
+            firstName: 'Jacob',
+            lastName: 'Thornton',
+            username: '@fat',
+            email: 'fat@yandex.ru',
+            age: '45',
+            status: 'primary'
+          },
+          {
+            id: 3,
+            firstName: 'Larry',
+            lastName: 'Bird',
+            username: '@twitter',
+            email: 'twitter@outlook.com',
+            age: '18',
+            status: 'success'
+          },
+          {
+            id: 4,
+            firstName: 'John',
+            lastName: 'Snow',
+            username: '@snow',
+            email: 'snow@gmail.com',
+            age: '20',
+            status: 'danger'
+          },
+          {
+            id: 5,
+            firstName: 'Jack',
+            lastName: 'Sparrow',
+            username: '@jack',
+            email: 'jack@yandex.ru',
+            age: '30',
+            status: 'warning'
+          }
         ]
       }
     }
