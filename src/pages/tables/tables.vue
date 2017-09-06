a43d524c212f17f59586d3f302bc5f75be7444dd
Rumpel <hades137747575@gmail.com>
2017-09-06 13:50:25 +0800

    Contextual Table

--- src/pages/tables/tables.vue
+++ src/pages/tables/tables.vue
@@ -117,9 +117,11 @@
         </div>
       </vue-panel>
     </div>
-    <div class="row">
-      <div class="col-lg-6 col-md-12">
-        <vue-panel vue-panel-title="Contextual Table" class="with-scroll table-panel">
+  </div>
+  <div class="row">
+    <div class="col-lg-6 col-md-12">
+      <vue-panel vue-panel-title="Contextual Table" class="with-scroll table-panel">
+        <div>
           <table class="table">
             <tr>
               <th>#</th>
@@ -171,8 +173,8 @@
               <td>30</td>
             </tr>
           </table>
-        </vue-panel>
-      </div>
+        </div>
+      </vue-panel>
     </div>
   </div>
 </div>
