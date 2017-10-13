b6320ae4d1c84b283d49cc61551548fabe0d9b53
Rumpel <hades137747575@gmail.com>
2017-10-13 13:07:01 +0800

    add http

--- src/pages/httpClient/http.vue
+++ src/pages/httpClient/http.vue
@@ -1,12 +1,39 @@
 <template>
   <div>
     http test
+    <button @click="get()">
+      get
+    </button>
+    <button @click="post()">
+      post
+    </button>
   </div>
 </template>
 
 <script>
   export default {
-
+    methods: {
+      get () {
+        this.$http.get('/someUrl').then(response => {
+          console.log('success')
+          console.log(response)
+        }, response => {
+          // error callback
+          console.log('error')
+          console.log(response)
+        })
+      },
+      post () {
+        this.$http.post('/someUrl').then(response => {
+          console.log('success')
+          console.log(response)
+        }, response => {
+          // error callback
+          console.log('error')
+          console.log(response)
+        })
+      }
+    }
   }
 </script>
 
